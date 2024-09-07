88df7fa04307c33b7a51774805f7705aaa0d3a88
sathninduk <45420836+sathninduk@users.noreply.github.com>
2024-09-07 10:53:52 +0530

    feat: openjml

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public class Main {
         hospital.addPatient();
         hospital.addPatient();
 
-        // Remove 1 patient hi
+        // Remove 1 patient
         hospital.removePatient();
 
         // Print patient count
@@ -20,26 +20,43 @@ class Hospital {
     private int patientCount = 0;
 
     // Constructor
+    /*@ public normal_behavior
+      @ ensures patientCount == 0;
+      @*/
     public Hospital() {
         patientCount = 0;
     }
 
     // Getters
+    /*@ public normal_behavior
+      @ ensures \result == patientCount;
+      @*/
     public int getPatientCount() {
         return patientCount;
     }
 
     // Setters
+    /*@ public normal_behavior
+      @ requires patientCount >= 0;
+      @ ensures this.patientCount == patientCount;
+      @*/
     public void setPatientCount(int patientCount) {
         this.patientCount = patientCount;
     }
 
     // Increment patientCount by 1
+    /*@ public normal_behavior
+      @ ensures patientCount == \old(patientCount) + 1;
+      @*/
     public void addPatient() {
         patientCount++;
     }
 
     // Decrement patientCount by 1
+    /*@ public normal_behavior
+      @ requires patientCount > 0;
+      @ ensures patientCount == \old(patientCount) - 1;
+      @*/
     public void removePatient() {
         patientCount--;
     }
