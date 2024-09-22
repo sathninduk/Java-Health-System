af1e607e1155f1fcc145a979a44995e96d4b69d2
sathninduk <45420836+sathninduk@users.noreply.github.com>
2024-09-22 18:08:11 +0530

    rm jml for 3 classes

--- Animal/Birds/Crow.java
+++ Animal/Birds/Crow.java
@@ -1,51 +1,77 @@
+/**
+ * User Acceptance Criteria for Crow Class
+ *
+ * 1. Initialization
+ *    - Given a new `Crow` object is created with a specific color,
+ *    - When the `Crow` object is initialized,
+ *    - Then the `color` attribute should be set to the provided color,
+ *    - And the `age` attribute should be initialized to 0.
+ *
+ * 2. Get Color
+ *    - Given a `Crow` object with a specific color,
+ *    - When the `getColor` method is called,
+ *    - Then it should return the current color of the crow.
+ *
+ * 3. Set Color
+ *    - Given a `Crow` object,
+ *    - When the `setColor` method is called with a new color,
+ *    - Then the `color` attribute should be updated to the new color.
+ *
+ * 4. Increment Age
+ *    - Given a `Crow` object with a specific age,
+ *    - When the `incrementAge` method is called,
+ *    - Then the `age` attribute should be incremented by 1.
+ *
+ * 5. Decrement Age
+ *    - Given a `Crow` object with a specific age,
+ *    - When the `decrementAge` method is called,
+ *    - Then the `age` attribute should be decremented by 1,
+ *    - And the `age` attribute should not be less than 0.
+ *
+ * 6. Get Age
+ *    - Given a `Crow` object with a specific age,
+ *    - When the `getAge` method is called,
+ *    - Then it should return the current age of the crow.
+ *
+ * 7. Set Age
+ *    - Given a `Crow` object,
+ *    - When the `setAge` method is called with a new age,
+ *    - Then the `age` attribute should be updated to the new age,
+ *    - And the `age` attribute should not be less than 0.
+ */
+
 package Animal.Birds;
 
 public class Crow {
     private String color;
     private int age;
 
-    /*@ public invariant age >= 0; @*/
-
-    /*@ requires color != null;
-      @ ensures this.color.equals(color);
-      @*/
     public Crow(String color) {
         this.color = color;
     }
 
-    /*@ ensures \result.equals(color); @*/
-    public /*@ pure @*/ String getColor() {
+    public String getColor() {
         return color;
     }
 
-    /*@ ensures age == \old(age) + 1; @*/
     public void incrementAge() {
         age++;
     }
 
-    /*@ ensures age == \old(age) - 1; @*/
     public void decrementAge() {
         age--;
     }
 
-    /*@ ensures \result == age; @*/
-    public /*@ pure @*/ int getAge() {
+    public int getAge() {
         return age;
     }
 
-    /*@ requires age >= 0;
-      @ ensures this.age == age;
-      @*/
     public void setAge(int age) {
         this.age = age;
     }
 
-    /*@ requires color != null;
-      @ ensures this.color.equals(color);
-      @*/
     public void setColor(String color) {
         this.color = color;
         // change
-        
     }
 }
--- Animal/Cat.java
+++ Animal/Cat.java
@@ -4,45 +4,30 @@ public class Cat {
     private String color;
     private int age;
 
-    /*@ public invariant age >= 0; @*/
-
-    /*@ requires color != null;
-      @ ensures this.color.equals(color);
-      @*/
     public Cat(String color) {
         this.color = color;
     }
 
-    /*@ ensures \result.equals(color); @*/
-    public /*@ pure @*/ String getColor() {
+    public String getColor() {
         return color;
     }
 
-    /*@ ensures age == \old(age) + 1; @*/
     public void incrementAge() {
         age++;
     }
 
-    /*@ ensures age == \old(age) - 1; @*/
     public void decrementAge() {
         age--;
     }
 
-    /*@ ensures \result == age; @*/
-    public /*@ pure @*/ int getAge() {
+    public int getAge() {
         return age;
     }
 
-    /*@ requires age >= 0;
-      @ ensures this.age == age;
-      @*/
     public void setAge(int age) {
         this.age = age;
     }
 
-    /*@ requires color != null;
-      @ ensures this.color.equals(color);
-      @*/
     public void setColor(String color) {
         this.color = color;
     }
